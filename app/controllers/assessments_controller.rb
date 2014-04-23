36a6eb415d3c4a95a08198d693c5371452787ce3
Pito Salas <pitosalas@gmail.com>
2014-04-22 21:33:04 -0400

    first draft

---
+++ app/controllers/assessments_controller.rb
@@ -0,0 +1,8 @@
+class AssessmentsController < ApplicationController
+	def assess
+	end
+
+	def submit_assessments
+
+	end
+end	
---
+++ test/integration/null_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+describe "Null Test" do
+	it "always works" do
+		1.must_equal 1
+	end
+end
