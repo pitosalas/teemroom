f18009c3009435c9d7e62713fc39ab604f94bf54
Pito Salas <pitosalas@gmail.com>
2014-04-30 21:01:58 -0400

    added cert as an experiemtn

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,8 +6,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, Rails.application.secrets.google_client_id,
                      Rails.application.secrets.google_client_secret,
                      {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
-end
-
   puts "********** Google OAuth Info: #{Rails.application.secrets.google_client_id} and #{Rails.application.secrets.google_client_secret}"
 
 end
